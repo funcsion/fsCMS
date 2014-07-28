afa5da3b625300b97432fc3f46fcc11cd6841280
fungzheng <271919945@qq.com>
2014-07-29 03:15:42 +0800

    fix banner

--- themes/default/views/_include/home_banner.php
+++ themes/default/views/_include/home_banner.php
@@ -1,54 +1,29 @@
 <div class="slide-banner">
-    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407011404212509.jpg)"></div>
-    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405495216.jpg)"></div>
-    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405495172.jpg)"></div>
-    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405494758.jpg)"></div>
-    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405494991.jpg)"></div>
+    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_baseUrl ?>/images/banner/201407011404212509.jpg)"></div>
+    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_baseUrl ?>/images/banner/201407161405495216.jpg)"></div>
+    <div class="banner-bg" style="display: block; background-image: url(<?php echo $this->_baseUrl ?>/images/banner/banner.jpg)"></div>
     <div class="banner-content">
-
-
-        <a class="banner" data-bg="<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407011404212509.jpg" href="event_view_218.html" title="中山丘比特婚纱摄影" target="_blank">
+        <a class="banner" data-bg="<?php echo $this->_baseUrl ?>/images/banner/201407011404212509.jpg)" href="event_view_218.html" title="中山丘比特婚纱摄影" target="_blank">
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
         </a>
-        <a class="banner" data-bg="<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405495216.jpg" href="works.html"  title="页头广告4" target="_blank">
+        <a class="banner" data-bg="<?php echo $this->_baseUrl ?>/images/banner/201407161405495216.jpg)" href="works.html"  title="页头广告4" target="_blank">
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
         </a>
-        <a class="banner" data-bg="<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405495172.jpg" href="works.html"  title="页头广告3" target="_blank">
+        <a class="banner" data-bg="<?php echo $this->_baseUrl ?>/images/banner/banner.jpg)" href="works.html"  title="页头广告3" target="_blank">
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
             <div class="banner-img" style="left:0"></div>
         </a>
-        <a class="banner" data-bg="<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405494758.jpg" href="news.html" title="页头广告2" target="_blank">
-            <div class="banner-img" style="left:0"></div>
-            <div class="banner-img" style="left:0"></div>
-            <div class="banner-img" style="left:0"></div>
-        </a>
-        <a class="banner" data-bg="<?php echo $this->_theme->baseUrl?>/upload/small/201407/201407161405494991.jpg" href="" title="中山丘比特婚纱摄影" target="_blank">
-            <div class="banner-img" style="left:0"></div>
-            <div class="banner-img" style="left:0"></div>
-            <div class="banner-img" style="left:0"></div>
-        </a>
-
-
-
-
     </div>
     <div class="banner-nav-bg"></div>
     <ul class="banner-nav">
-
-
         <li class="active"></li>
         <li ></li>
         <li ></li>
-        <li ></li>
-        <li ></li>
-
-
-
     </ul>
     <div class="banner-bar-bg">
         <div class="banner-bar"></div>
